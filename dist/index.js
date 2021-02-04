71f86de762984f036d0c462285b2f759b8cf1423
teppeis <teppeis@gmail.com>
2021-02-04 23:19:56 +0900

    add typesVersions

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+function hello() {
+    return "hello!";
+}
+exports.hello = hello;
---
+++ dist/not-exported.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bar = void 0;
+function bar() {
+    return "bar!";
+}
+exports.bar = bar;
